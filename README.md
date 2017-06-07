b54d3399186313b4f0935b755705a6cc2bc0a1bd
Kelvin <273degreeskelvin@gmail.com>
2017-06-07 11:56:03 +1000

    Scrapes and saves

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 #youstats
+This web scraper crawls around Youtube collecting analytics from each video.
+The information collected includes view-count, likes, dislikes, title and
+channel.
+
+##Usage
+`python scraper.py` begins running the scraper. `Ctrl + c` to terminate while
+running. This save whatever progress has been made to pickle dumps:
+- **data_store** is a list of dictionaries with all the scraped information.
+- **active** is the list of addresses queued for scraping.
+- **explored** is the list of addresses that have already been scraped.
+
+##Todo
+- [ ] check for dropped request
+- [ ] resend dropped request
+- [ ] load previous saves
+- [ ] module for analysing the stats
+- [ ] NLP module for the title
+- [ ] scrape comments
+- [ ] generalise scraping algorithm
+
+##Requirements
+- BeautifulSoup4
+- Matplotlib
+- Seaborn
+- Pandas
--- scraper.py
+++ scraper.py
@@ -1,17 +1,19 @@
 from urllib.request import urlopen
 import bs4
 from bs4 import re
+import signal
+import pickle
 
 base_url = "https://www.youtube.com"
 
 def make_dict(stem_url):
     soup = make_soup(base_url + stem_url)
     out_dict = {
-        "name" : get_name(soup),
-        "channel" : get_channel(soup),
-        "views" : get_views(soup),
-        "lnds" : get_LnDs(soup),
-        "rec" : get_recommendeds(soup),
+        "name" : my_catch(base_url + stem_url, get_name, soup),
+        "channel" : my_catch(base_url + stem_url, get_channel, soup),
+        "views" : my_catch(base_url + stem_url, get_views, soup),
+        "lnds" : my_catch(base_url + stem_url, get_LnDs, soup),
+        "recommended" : my_catch(base_url + stem_url, get_recommended, soup),
     }
     return out_dict
 
@@ -24,7 +26,7 @@ def get_name(soup):
 def get_channel(soup):
     return soup.find("div", {"class" : "yt-user-info"}).find("a").text
 
-def get_recommendeds(soup):
+def get_recommended(soup):
     return [i.attrs["href"] for i in soup.find_all("a",{"class" : "content-link"})]
 
 def get_views(soup):
@@ -37,10 +39,27 @@ def get_LnDs(soup):
     sentiments from the soup of a given video page
     """
     button_content = soup.find_all("span", {"class" : "yt-uix-button-content"})
-    lnds = even_indecies([i.text for i in button_content if i.text.isnumeric()])
-    assert len(lnds) == 2
+    cleaned_texts = [i.text.replace(",","") for i in button_content]
+    lnds = even_indecies([i for i in cleaned_texts if i.isnumeric()])
+    if len(lnds) != 2:
+        # print("Error found on", get_name(soup))
+        # [print(i.text) for i in button_content]
+        # print("======== to ========")
+        # [print(i) for i in cleaned_texts]
+        # print([i for i in cleaned_texts if i.isnumeric()])
+        # print("======== to ========")
+        # print(lnds)
+        raise IndexError("size mismatch")
+        # return None
     ints = [int(i) for i in lnds]
-    return (ints[0], ints[1], ints[0] / sum(ints))
+    return (ints[0], ints[1], safe_ratio(ints))
+
+
+def safe_ratio(ints):
+    if sum(ints) == 0:
+        return None
+    else:
+        return ints[0] / sum(ints)
 
 def even_indecies(lst):
     return [lst[i] for i in range(len(lst)) if i % 2 == 0]
@@ -49,7 +68,44 @@ def even_indecies_aux(lst):
     """faster but a touch less readable"""
     return [lst[i] for i in range((len(lst) + 1) // 2)]
 
+def cb_sigint_handler(signum, stack):
+    global run
+    print("SIGINT received")
+    run = False
+
+def my_catch(url, f, soup):
+    try:
+        return f(soup)
+    except Exception as e:
+        print(url, repr(f), e)
+        return None
+
 if __name__ == '__main__':
-    stem = "/watch?v=VGCE_3fjzU4"
+    data_store = []
+    active = ["/watch?v=VGCE_3fjzU4"]
+    explored = []
     # soup = make_soup(base_url + "/watch?v=VGCE_3fjzU4")
-    print(make_dict(stem))
+    # print(make_dict(stem))
+    run = True
+    signal.signal(signal.SIGINT, cb_sigint_handler)
+    while run and len(active) > 0:
+        try:
+            temp_url = active.pop()
+            temp_dict = make_dict(temp_url)
+            explored.append(temp_url)
+            for rec in temp_dict["recommended"]:
+                if not (rec in explored or rec in active):
+                    active.append(rec)
+            data_store.append(temp_dict)
+        except Exception as e:
+            print("this happened", e)
+            run = False
+
+    print("length of data_store:", len(data_store))
+
+    with open("data_store", "wb") as f:
+        pickle.dump(data_store, f)
+    with open("active", "wb") as g:
+        pickle.dump(active, g)
+    with open("explored", "wb") as h:
+        pickle.dump(explored, h)
